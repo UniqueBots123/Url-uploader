cb536beb3c2b96fd2bedf9e90a6e63d02f5148ea
UniqueBots123 <uniquebots123@gmail.com>
2024-12-29 20:13:14 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,11 +11,11 @@ logging.basicConfig(
 
 class Config(object):
     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7842208500:AAGIE8C41yZ4fJeTd-VDa2SpTr6tiJ6NvLI")
     
-    API_ID = int(os.environ.get("API_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "25888462"))
     
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "50620d85a99c79e74972df60f0a11da1")
     
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     
@@ -37,21 +37,21 @@ class Config(object):
     
     PROCESS_MAX_TIMEOUT = 0
     
-    DEF_WATER_MARK_FILE = "LegendSources"
+    DEF_WATER_MARK_FILE = ""
     
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Unique:n219219219@unique.p4hpd.mongodb.net/?retryWrites=true&w=majority&appName=Unique")
     
-    SESSION_NAME = os.environ.get("SESSION_NAME", "LegendSources")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Nothingbots")
     
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002206709779"))
     
     LOGGER = logging
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002446244647")
     
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "7766469355"))
     
     TG_MIN_FILE_SIZE = 2097152000
     
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "nothing_url_uploader_bot")
                                   
